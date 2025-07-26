54bf6e6f79dea3f34988af0b0d751a48a9a0ad48
Adriel <adrielamaral.github@gmail.com>
2025-07-25 22:33:08 -0300

    Exercício Fahrenheit Para Celsius

---
+++ fahrenheitParaCelsius.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class fahrenheitParaCelsius {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Informe a temperatura em F°: ");
+        float F = input.nextFloat();
+
+        input.close();
+
+        float C = ((F - 32) * 5) / 9;
+        System.out.println("CONVERTENDO TEMPERATURA...\n");
+        System.out.printf("%.1f° Fahrenheit é igual a: %.1f° Celsius%n", F, C);
+    }
+}
