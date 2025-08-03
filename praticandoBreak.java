ac2679db4c096ec9b9298d91a05e468ce6f256f9
Adriel <adrielamaral.github@gmail.com>
2025-08-03 14:10:18 -0300

    Praticando break

---
+++ praticandoBreak.java
@@ -0,0 +1,10 @@
+public class praticandoBreak {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100000; i++) {
+            if (i % 17 == 0 && i % 23 == 0 && i % 36 == 0) {
+                System.out.println("O primeiro número divisivel por 17, 23 e 36 é: " + i);
+                break;
+            }
+        }
+    }
+}
