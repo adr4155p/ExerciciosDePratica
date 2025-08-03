c5f4cc28f96f11400ce3f78c4832aae204560981
Adriel <adrielamaral.github@gmail.com>
2025-08-03 14:10:00 -0300

    Praticando Continue

---
+++ PraticandoContinue.java
@@ -0,0 +1,10 @@
+public class PraticandoContinue {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 30; i++) {
+            if (i % 4 == 0) {
+                continue;
+            }
+            System.out.println(i);
+        }
+    }
+}
