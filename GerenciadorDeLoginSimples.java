20c22cc863e5d491d5fc0a161a8f9932154ec0cb
Adriel <adrielamaral.github@gmail.com>
2025-08-17 14:45:49 -0300

    Exercício: Gerenciador de login simples.

---
+++ GerenciadorDeLoginSimples.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class GerenciadorDeLoginSimples {
+    public static void main(String[] args) {
+
+        String usuarioCorreto = "admin";
+        String senhaCorreta = "12345";
+
+        boolean loginSucesso = false;
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Tentativa " + (i + 1) + " de 3:");
+            System.out.print("Digite seu nome de usuário: ");
+            String usuarioDigitado = scanner.next();
+
+            System.out.print("Digite sua senha: ");
+            String senhaDigitada = scanner.next();
+
+            if (usuarioDigitado.equals(usuarioCorreto) && senhaDigitada.equals(senhaCorreta)) {
+                System.out.println("Login bem-sucedido! Bem-vindo.");
+                loginSucesso = true;
+                break;
+            } else {
+                System.out.println("Usuário ou senha incorretos. Tente novamente.");
+            }
+        }
+        if (!loginSucesso) {
+            System.out.println("Suas 3 tentativas terminaram. O programa será encerrado.");
+        }
+
+        scanner.close();
+    }
+}
